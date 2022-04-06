8a3018402ed3f5d8234be7e8bf1d57e4b80216e6
Anu and Sri Kemburu <avisfy0@hotmail.com>
2022-04-05 22:20:55 -0400

    working

--- controllers/records.js
+++ controllers/records.js
@@ -2,20 +2,39 @@ const express = require('express');
 const router = express.Router();
 const Records = require('../models/records.js');
 
-router.get('/', (req, res)=>{
-    res.send('index');
-});
-
 // find all documents
 // executes, passing results to callback
 
+router.get('/', (req, res)=>{
+    Records.find({}, (err, foundRecords)=>{
+        res.json(foundRecords);
+    });
+});
 
-// router.get('/', (req, res)=>{
-//     Records.find({}, (err, foundRecords)=>{
-//         res.json(foundRecords);
-//     });
-// });
+router.get('/:id', (req, res)=>{
+    Records.findById(req.params.id, (err, foundRecord)=>{
+        res.json(foundRecord);
+    });
+});
 
+router.delete('/:id', (req, res)=>{
+    Records.findByIdAndRemove(req.params.id, (err, deletedRecord)=>{
+        res.json(deletedRecord);
+    });
+});
 
+//...farther down the page
+
+router.post('/', (req, res)=>{
+    Records.create(req.body, (err, createdRecord)=>{
+        res.json(createdRecord); //.json() will send proper headers in response so client knows it's json coming back
+    });
+});
+
+router.put('/:id', (req, res)=>{
+    Records.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedRecord)=>{
+        res.json(updatedRecord);
+    });
+});
 
 module.exports = router;
